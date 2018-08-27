80ffa0dcaffe4f80e07ce6c91ff7beea82d73077
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-08-27 20:46:42 +0530

    Update and rename README.md to Array_With_Index.java

---
+++ Array_With_Index.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+  public class Array_With_Index{
+    public cstatic void main(String args[]){
+      Scanner Sc=new Scanner(System.in);
+      System.out.println("Enter Array Size:");
+      int Array_Size=sc.nextInt();
+      int Array_value[]=new int[Array_Size];
+        for(int i=0;i<Array_Size;i++)
+        {
+          Array_value[i]=sc.nexInt();
+        }
+        for(int i=0;i<Array_Size;i++)
+        {
+          System.out.println((i+1)+". "+Array_value[i]);
+        }
+      }  
+    }  
